24f5456ed236b31f5ccdcf1d8597f26f499a302d
Daniel Tamunotonye <dan_tonye@Gudi.local>
2021-04-03 00:37:42 -0400

    adjust relations and also added upcoming events

--- controllers/outdoors.js
+++ controllers/outdoors.js
@@ -1,5 +1,4 @@
-const Outdoor = require("../models/outdoor")
-const user = require("../models/user")
+const Outdoor = require("../models/outdoorModel")
 const User = require("../models/user")
 
 module.exports = {
@@ -65,29 +64,52 @@ async function addGoingEvent(req, res) {
             const user = req.user
             const userId = req.user._id
             const eventId = req.params.id
+            let foundEvent;
 
-            
-            // push the event to the list of upcoming events
-            user.upcoming.push({event: eventId})
-            // add user to attendees
-            console.log(eventId)
+
+            // a simple function that will be called when a condition is met or not
+            const addEventAndUser = () => {
+                // add user to attendees
             Outdoor.findById(eventId, (err, outdoor) => {
                 outdoor.Attendant.push({user: userId})
                 outdoor.save()
             })
+             // push the event to the list of upcoming events
+             user.upcoming.push({event: eventId})
+                return user.save((err) =>{
+                    if(err) { 
+                        console.log(err)  
+                    }else{  
+                        res.redirect('/')
+                    }
+                })
+            }
 
+            // incase of an empty list of upcoming events
+            if(user.upcoming.length === 0){
+                addEventAndUser()
+            }
 
-
-            return user.save((err) =>{
-                if(err) { 
-                    console.log(err)  
-                }else{  
-                    // res.redirect('/')
+            //  if  user has event in upcoming
+            for (let i=0; i < user.upcoming.length; i++) {
+                console.log(i)
+                if (user.upcoming[i].event._id.toString() === eventId.toString()) {
+                    foundEvent = user.upcoming[i]
                 }
-            })
+            } 
+
+            // if user is not in the event
+            if(!foundEvent){
+                addEventAndUser()
+            }
+
+            // if user is in the event
+            if(foundEvent){
+               return res.redirect('/stay_connected/all_events');
+                // send a message to the user
+            }
         }
-        
-        // console.log(user)
+
         
 
     }catch(err){
@@ -99,10 +121,18 @@ async function addGoingEvent(req, res) {
 // get the user upcoming events
 async function upcoming(req, res) {
     try{
-        // display the user upcoming events here
-        res.render("outdoors/upcoming", {
-            user: req.user,
+
+        await User.findById(req.user._id).populate('upcoming.event').exec( (err, user) => {
+            const upcoming = user.upcoming
+            console.log(upcoming)
+            res.render("outdoors/upcoming", {
+                user: req.user,
+                upcoming: upcoming
+            })
         })
+
+        // display the user upcoming events here
+        
     }catch(err){
         console.log(err)
     }
