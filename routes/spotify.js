51cd38251f64ae0ceb045f75f34df978370672c9
Dor <dorzairi@ymail.com>
2021-04-03 14:39:05 -0400

    pushing for the sake of push

--- routes/spotify.js
+++ routes/spotify.js
@@ -1,12 +1,24 @@
 var express = require("express");
 var router = express.Router();
 
+const fetch = require("node-fetch");
 const request = require("request");
 
-const token = process.env.token;
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
 
-router.get("/", function(req, res, next) {
+const rootURL = "https://api.spotify.com/";
+
+router.get("/", async function(req, res, next) {
   const username = req.query.username;
+
   console.log(username);
-  res.render("index");
+
+  await fetch("${rootURL}v1/me")
+    .then(res => res.json())
+    .then(userData => {
+      res.render("spotify", { userData });
+    });
 });
+
+module.exports = router;
